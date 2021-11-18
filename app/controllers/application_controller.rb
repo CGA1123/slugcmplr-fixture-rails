7aa879fe65bc02ca81a2fcaa6cca0424abec6882
Christian Gregg <christian@bissy.io>
2021-11-18 10:16:40 +0000

    example

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def foo
+    if params[:bad]
+      raise "hello"
+    else
+      render json: { hello: "foo" }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/foo', to: 'application#foo', defaults: { format: :json }
 end
